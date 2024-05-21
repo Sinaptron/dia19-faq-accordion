c650053abb50d3ae1ce2c7e0d9fdce046b534cf1
Sinaptron <126521279+Sinaptron@users.noreply.github.com>
2024-05-21 17:53:14 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,59 @@
+function mostrarContenido() {
+    var contenido = document.getElementById("contenidoOculto");
+    var iconoImagen = document.getElementById("iconoImagen");
+
+    if (contenido.style.display ==='none') {
+      // Si el contenido está oculto, lo mostramos y cambiamos el icono
+      contenido.style.display ='block';
+      iconoImagen.src = "images/icon-minus.svg";
+    } else {
+      // Si el contenido está visible, lo ocultamos y restauramos el icono original
+      contenido.style.display ='none';
+      iconoImagen.src = "images/icon-plus.svg";
+    }
+  }
+
+function mostrarContenido2() {
+    var contenido = document.getElementById("contenidoOculto2");
+    var iconoImagen = document.getElementById("iconoImagen2");
+
+    if (contenido.style.display === "none") {
+      // Si el contenido está oculto, lo mostramos y cambiamos el icono
+      contenido.style.display = "block";
+      iconoImagen.src = "images/icon-minus.svg";
+    } else {
+      // Si el contenido está visible, lo ocultamos y restauramos el icono original
+      contenido.style.display = "none";
+      iconoImagen.src = "images/icon-plus.svg";
+    }
+  }
+
+function mostrarContenido3() {
+    var contenido = document.getElementById("contenidoOculto3");
+    var iconoImagen = document.getElementById("iconoImagen3");
+
+    if (contenido.style.display === "none") {
+      // Si el contenido está oculto, lo mostramos y cambiamos el icono
+      contenido.style.display = "block";
+      iconoImagen.src = "images/icon-minus.svg";
+    } else {
+      // Si el contenido está visible, lo ocultamos y restauramos el icono original
+      contenido.style.display = "none";
+      iconoImagen.src = "images/icon-plus.svg";
+    }
+  }
+
+function mostrarContenido4() {
+    var contenido = document.getElementById("contenidoOculto4");
+    var iconoImagen = document.getElementById("iconoImagen4");
+
+    if (contenido.style.display === "none") {
+      // Si el contenido está oculto, lo mostramos y cambiamos el icono
+      contenido.style.display = "block";
+      iconoImagen.src = "images/icon-minus.svg";
+    } else {
+      // Si el contenido está visible, lo ocultamos y restauramos el icono original
+      contenido.style.display = "none";
+      iconoImagen.src = "images/icon-plus.svg";
+    }
+  }
